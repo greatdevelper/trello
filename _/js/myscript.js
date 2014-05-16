5d1dd0eb5a098810a6fff882717ab57f32005e69
Abass Zakari <developer.meltwater@gmail.com>
2014-05-16 16:17:51 -0700

    Initial commit

---
+++ _/js/myscript.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$("#home a:contains('Home')").parent().addClass('active');
+	$("#details a:contains('Details')").parent().addClass('active');
+	$("#about a:contains('About')").parent().addClass('active');
+	//$("#home a:contains('Home')").parent().addClass('active');
+	//$("#home a:contains('Home')").parent().addClass('active'); 
+
+	$.backstretch("images/background/bg7.jpg");
+});
